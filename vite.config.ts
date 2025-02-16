160b6842f32916145991663eb1a407a2f3b07450
CrinGelele <152797591+CrinGelele@users.noreply.github.com>
2025-02-16 18:18:36 +0300

    Tauri start

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://192.168.1.80:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
