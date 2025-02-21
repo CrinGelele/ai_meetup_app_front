fe3c1c721ac9c3378ee598ddb51326df2976e150
CrinGelele <152797591+CrinGelele@users.noreply.github.com>
2025-02-21 09:01:03 +0300

    readme

--- Routes.tsx
+++
@@ -1,9 +0,0 @@
-export const ROUTES = {
-    HOME: "/",
-    SPEAKERS: "/speakers",
-  }
-  export type RouteKeyType = keyof typeof ROUTES;
-  export const ROUTE_LABELS: {[key in RouteKeyType]: string} = {
-    HOME: "Главная",
-    SPEAKERS: "Спикеры",
-  };
